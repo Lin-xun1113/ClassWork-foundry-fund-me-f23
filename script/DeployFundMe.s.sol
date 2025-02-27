d346734bafdf1f701bb88564c264a59cf33ebbf9
Linxun <linxun1113@gmail.com>
2025-02-27 23:05:52 +0800

    our first commit!

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,14 +1,18 @@
-// SPDX_License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        // Get the price feed address from the NetworkConfig
+        address ethUsdPriceFeed = helperConfig.getActiveNetworkConfig();
         vm.startBroadcast();
-        FundMe fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundme = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundme;
     }
